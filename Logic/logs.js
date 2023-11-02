75874c9098759c97e27c6ca4fb700efe5e5e9592
Noah Jaeger <nosajaeger@gmail.com>
2023-11-01 17:32:42 -0700

    fixed some bugs

--- Logic/logs.js
+++ Logic/logs.js
@@ -6,6 +6,7 @@ function create_table() {
     var table = document.getElementById("result_table");
     table.innerHTML = "";
     
+    var preview_length = 150;
     var start_date = 0;
     var end_date = 0;
     
@@ -32,45 +33,53 @@ function create_table() {
         console.log(this);
     }
     
-    /**
-    *Function that accesses Database via Ajax and builds and appends the table data 
-    */
-    xhttp.onload = function() {
-        JSON.parse(this.responseText).forEach(function (log) {
-            var subject_line = document.createElement("tr");
-            subject_line.innerHTML = 
-                "<td>" + log.subject + "</td>\n" +
-                "<td>" +log.date_sent + "</td>\n" +
-                "<td>" +log.time_sent + "</td>\n" +
-                "<td>" +log.username + "</td>\n" +
-                "<td>" +log.num_subs + "</td>\n";
-            table.appendChild(subject_line);
-            
-            var message_body = document.createElement("tr");
-            message_body.innerHTML = 
-                '<td colspan="5" class="message_body">' + log.body.substr(0, 100).trim() + '...' + '</td>\n';
-            
-            //set data for expanding and shortening the message body
-            message_body.setAttribute("data-body", log.body);
-            message_body.setAttribute("data-prev", 1);
-            
-            //add function to expand and shorten the message text
-            message_body.addEventListener("click", function (){
-                if(this.getAttribute("data-prev") == 1) {
-                    this.firstElementChild.innerHTML = this.getAttribute("data-body");
-                    this.setAttribute("data-prev", 0);
-                }else {
-                    this.firstElementChild.innerHTML = this.getAttribute("data-body").substr(0, 100).trim() + '...';
-                    this.setAttribute("data-prev", 1);
-                }
-            });
-            
-            table.appendChild(message_body);
-        });
-    }
+    if(!(Number((document.getElementById("start_date").value).replaceAll("-", "")) > Number(document.getElementById("end_date").value.replaceAll("-", "")) ) || end_date == 0) {
     
-    xhttp.open("GET", `../Data/fetch_logs.php?start_date=${start_date}&end_date=${end_date}`);
-    xhttp.send();
+        /**
+        *Function that accesses Database via Ajax and builds and appends the table data 
+        */
+            xhttp.onload = function() {
+                try {
+                JSON.parse(this.responseText).forEach(function (log) {
+                    var subject_line = document.createElement("tr");
+                    subject_line.innerHTML = 
+                        "<td>" + log.subject + "</td>\n" +
+                        "<td>" +log.date_sent + "</td>\n" +
+                        "<td>" +log.time_sent + "</td>\n" +
+                        "<td>" +log.username + "</td>\n" +
+                        "<td>" +log.num_subs + "</td>\n";
+                    table.appendChild(subject_line);
+
+                    var message_body = document.createElement("tr");
+                    message_body.innerHTML = 
+                        '<td colspan="5" class="message_body">' + log.body.substr(0, preview_length).trim() + '<i>...</i>' + '</td>\n';
+
+                    //set data for expanding and shortening the message body
+                    message_body.setAttribute("data-body", log.body);
+                    message_body.setAttribute("data-prev", 1);
+
+                    //add function to expand and shorten the message text
+                    message_body.addEventListener("click", function (){
+                        if(this.getAttribute("data-prev") == 1) {
+                            this.firstElementChild.innerHTML = this.getAttribute("data-body");
+                            this.setAttribute("data-prev", 0);
+                        }else {
+                            this.firstElementChild.innerHTML = this.getAttribute("data-body").substr(0, preview_length).trim() + '<i>...</i>';
+                            this.setAttribute("data-prev", 1);
+                        }
+                    });
+
+                    table.appendChild(message_body);
+                }); } catch (error) {
+                    document.getElementById("dates_showing").innerHTML = "Error: encountered unexpected issue with database.";
+                }
+            }
+
+            xhttp.open("GET", `../Data/fetch_logs.php?start_date=${start_date}&end_date=${end_date}`);
+            xhttp.send();
+    } else {
+        document.getElementById("dates_showing").innerHTML = "Error: please enter a start date earlier than the end date.";
+    }
 }
 
 function init () {
