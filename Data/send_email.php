5575618be2f6c8ac3a146b0d562f3ac55f9507ba
Kyle Miller <millerkyle85@gmail.com>
2023-12-12 01:23:28 -0800

    fix sumbitForm bug

--- Data/send_email.php
+++ Data/send_email.php
@@ -3,16 +3,20 @@
 function sendEmail($to, $subject, $body, &$success) {
     $sender = "From: kyle.miller18@pcc.edu";
 
-// Validate the email address
+    // Validate the email address
     if (!filter_var($to, FILTER_VALIDATE_EMAIL)) {
         $success = false;
         return;
     }
 
-    $sender[] = 'MIME-Version: 1.0';
-    $sender[] = 'Content-type: text/html; charset=iso-8859-1';
+    $headers = [
+        'MIME-Version: 1.0',
+        'Content-type: text/html; charset=iso-8859-1',
+        $sender,
+    ];
+
     // Send the email without the Bcc header
-    if (mail($to, $subject, $body, implode("\r\n", $sender))) {
+    if (mail($to, $subject, $body, implode("\r\n", $headers))) {
         // Log the email details
         $logMessage = "To: $to, Subject: $subject, Body: $body";
         error_log($logMessage);
@@ -22,8 +26,6 @@ function sendEmail($to, $subject, $body, &$success) {
         // If the email fails, set success to false
         $success = false;
     }
-
-
 }
 
 
